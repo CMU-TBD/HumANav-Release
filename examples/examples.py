b0dba76dc1ef323a1778de548bf273f87ff757cf
GustavoSilvera <gustavo.r.silvera@gmail.com>
2020-05-21 16:16:45 -0700

    added generalization for 10 random humans

--- examples/examples.py
+++ examples/examples.py
@@ -5,6 +5,9 @@ from dotmap import DotMap
 from humanav.humanav_renderer import HumANavRenderer
 from humanav.renderer_params import create_params as create_base_params
 from humanav.renderer_params import get_surreal_texture_dir
+from random import seed 
+from random import random
+from random import randint
 
 def create_params():
     p = create_base_params()
@@ -100,41 +103,44 @@ def example1():
     # Convert the grid spacing to units of meters. Should be 5cm for the S3DIS data
     dx_m = dx_cm/100.
 
-    # Set the identity seed. This is used to sample a random human identity
-    # (gender, texture, body shape)
-    identity_rng = np.random.RandomState(48)
-    gus_identity_rng = np.random.RandomState(59)
-    
-    # Set the Mesh seed. This is used to sample the actual mesh to be loaded
-    # which reflects the pose of the human skeleton.
-    mesh_rng = np.random.RandomState(20)
-    gus_mesh_rng = np.random.RandomState(410)
-                                     
-    # State of the camera and the human. 
-    # Specified as [x (meters), y (meters), theta (radians)] coordinates
     camera_pos_13 = np.array([[7.5, 12., -1.3]])
-    human_pos_3 = np.array([8.0, 9.75, np.pi/2.])
-    gus_human_pos_3 = np.array([7.5, 9, np.pi/2.])
-    
-    # Speed of the human in m/s
-    human_speed = 0.7
+    humans = []#tuple of all the below
+    identity_rng = []
+    mesh_rng = []
+    human_pos_3 = []
+    human_speed = []
+    for i in range(10):
+        print("Generating human", i)
+        # Set the identity seed. This is used to sample a random human identity
+        # (gender, texture, body shape)
+        identity_rng.append(np.random.RandomState(randint(10, 100)))
 
-    humans = []
-    humans.append(tuple([gus_human_pos_3, human_speed, gus_identity_rng, gus_mesh_rng]))
-    humans.append(tuple([human_pos_3, human_speed, identity_rng, mesh_rng]))
+        # Set the Mesh seed. This is used to sample the actual mesh to be loaded
+        # which reflects the pose of the human skeleton.
+        mesh_rng.append(np.random.RandomState(randint(10, 100)))
+
+        # State of the camera and the human. 
+        # Specified as [x (meters), y (meters), theta (radians)] coordinates
+        offset_x = 2 * random() - 1 #(-2, 2)
+        offset_y = 2 * random() - 1 #(-2, 2)
+        offset_theta = random() #(0, 1)
+        human_pos_3.append(np.array([8.0 + offset_x, 9.75 + offset_y, np.pi/2. + offset_theta]))
+    
+        # Speed of the human in m/s
+        human_speed.append(random())# random value from 0 to 1
+        humans.append(tuple([human_pos_3[i], human_speed[i], identity_rng[i], mesh_rng[i]]))
     # Load a random human at a specified state and speed
     r.add_human_at_position_with_speed(humans)
 
     # Get information about which mesh was loaded
     human_mesh_info = r.human_mesh_params
-
     rgb_image_1mk3, depth_image_1mk1 = render_rgb_and_depth(r, camera_pos_13, dx_m, human_visible=True)
 
     # Remove the human from the environment
     r.remove_human()
 
     # Plot the rendered images
-    plot_images(rgb_image_1mk3, depth_image_1mk1, traversible, dx_m, camera_pos_13, [human_pos_3, gus_human_pos_3], 'example1.png')
+    plot_images(rgb_image_1mk3, depth_image_1mk1, traversible, dx_m, camera_pos_13, human_pos_3, 'example1.png')
 
 def get_known_human_identity(r):
     """
