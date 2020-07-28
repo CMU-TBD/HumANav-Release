c5c2dc6d182d3e2fc2ca9599af678daca22a5d54
GustavoSilvera <gustavo.r.silvera@gmail.com>
2020-07-28 09:11:47 -0700

    fixed tf unavailability

--- get_packages.sh
+++ get_packages.sh
@@ -10,9 +10,10 @@ set -e
 #checking that pip/conda are installed
 which pip || (echo -e "${RED}pip not installed${NC}" && exit)
 which conda || (echo -e "${RED}conda not installed${NC}" && exit)
-
+# need to install tf 1.10.1 which is no longer maintained via this link:
+#pip install https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.10.1.tar.gz
 #install pip packages
-PIP_PACKAGES="matplotlib dotmap pyassimp scikit-image scikit-fmm tensorflow==1.10.1 numpy==1.14.5"
+PIP_PACKAGES="matplotlib dotmap pyassimp scikit-image scikit-fmm numpy==1.14.5"
 echo -e "${CYAN}Using pip to install $PIP_PACKAGES ${NC}"
 for i in $PIP_PACKAGES; do
     pip install $i || (echo -e "${RED}Failed to install $i${NC}" && exit 1)
