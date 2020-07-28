efaeee809fdcff32cda7f1b9286369b28a70196e
GustavoSilvera <gustavo.r.silvera@gmail.com>
2020-07-28 09:31:55 -0700

    fixed get_packages to work with conda enrivonment

--- get_packages.sh
+++ get_packages.sh
@@ -10,10 +10,9 @@ set -e
 #checking that pip/conda are installed
 which pip || (echo -e "${RED}pip not installed${NC}" && exit)
 which conda || (echo -e "${RED}conda not installed${NC}" && exit)
-# need to install tf 1.10.1 which is no longer maintained via this link:
-#pip install https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.10.1.tar.gz
+
 #install pip packages
-PIP_PACKAGES="matplotlib dotmap pyassimp scikit-image scikit-fmm numpy==1.14.5"
+PIP_PACKAGES="matplotlib dotmap pyassimp scikit-image scikit-fmm numpy==1.14.5 tensorflow==1.10.1"
 echo -e "${CYAN}Using pip to install $PIP_PACKAGES ${NC}"
 for i in $PIP_PACKAGES; do
     pip install $i || (echo -e "${RED}Failed to install $i${NC}" && exit 1)
@@ -21,7 +20,7 @@ for i in $PIP_PACKAGES; do
 done
 
 #install conda packages
-CONDA_PACKAGES="pyopengl pandas"
+CONDA_PACKAGES="opencv pyopengl pandas"
 echo -e "${CYAN}Using conda to install $CONDA_PACKAGES ${NC}"
 for i in $CONDA_PACKAGES; do
     conda install -y $i || (echo -e "${RED}Failed to install $i${NC}" && exit 1)
