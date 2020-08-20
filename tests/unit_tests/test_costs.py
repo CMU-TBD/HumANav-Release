b0a2c35d69bfc0b2bab47274371942224ccc5c0c
GustavoSilvera <gustavo.r.silvera@gmail.com>
2020-08-19 18:48:46 -0700

    passed test_costs unit tests

--- tests/unit_tests/test_costs.py
+++ tests/unit_tests/test_costs.py
@@ -1,6 +1,4 @@
 import numpy as np
-import tensorflow as tf
-tf.enable_eager_execution()
 from costs.quad_cost_with_wrapping import QuadraticRegulatorRef
 from systems.dubins_v1 import DubinsV1
 from dotmap import DotMap
@@ -19,16 +17,19 @@ def create_params():
     p.linear_coeffs = np.array([0.0, 0.0, 0.0, 0.0, 0.0], dtype=np.float32)
     return p
 
+
 def create_system_dynamics_params():
     p = DotMap()
 
     p.simulation_params = DotMap(simulation_mode='ideal',
-                                 noise_params = DotMap(is_noisy=False,
-                                                       noise_type='uniform',
-                                                       noise_lb=[-0.02, -0.02, 0.],
-                                                       noise_ub=[0.02, 0.02, 0.],
-                                                       noise_mean=[0., 0., 0.],
-                                                       noise_std=[0.02, 0.02, 0.]))
+                                 noise_params=DotMap(is_noisy=False,
+                                                     noise_type='uniform',
+                                                     noise_lb=[-0.02, -
+                                                               0.02, 0.],
+                                                     noise_ub=[
+                                                         0.02, 0.02, 0.],
+                                                     noise_mean=[0., 0., 0.],
+                                                     noise_std=[0.02, 0.02, 0.]))
     return p
 
 
@@ -42,25 +43,27 @@ def test_quad_cost_with_wrapping():
     dubins = DubinsV1(p.dt, create_system_dynamics_params())
 
     goal = np.array([goal_x, goal_y, 0.], dtype=np.float32)
-    x_ref_nk3 = tf.constant(np.tile(goal, (n, k, 1)))
-    u_ref_nk2 = tf.constant(np.zeros((n, k, u_dim), dtype=np.float32))
+    x_ref_nk3 = np.array(np.tile(goal, (n, k, 1)))
+    u_ref_nk2 = np.array(np.zeros((n, k, u_dim), dtype=np.float32))
     trajectory_ref = dubins.assemble_trajectory(x_ref_nk3, u_ref_nk2)
 
     cost_fn = QuadraticRegulatorRef(trajectory_ref, dubins, p)
-    x_nk3 = tf.constant(np.zeros((n, k, x_dim), dtype=np.float32))
-    u_nk2 = tf.constant(np.zeros((n, k, u_dim), dtype=np.float32))
+    x_nk3 = np.array(np.zeros((n, k, x_dim), dtype=np.float32))
+    u_nk2 = np.array(np.zeros((n, k, u_dim), dtype=np.float32))
     trajectory = dubins.assemble_trajectory(x_nk3, u_nk2)
     cost_nk, _ = cost_fn.compute_trajectory_cost(trajectory)
 
-    cost = .5*(a*goal_x**2+a*goal_y**2)
-    assert(cost_nk.shape[0].value == n and cost_nk.shape[1].value == k)
-    assert((cost_nk.numpy() == cost).all())
+    cost = .5 * (a * goal_x**2 + a * goal_y**2)
+    assert(cost_nk.shape[0] == n and cost_nk.shape[1] == k)
+    assert((cost_nk == cost).all())
 
-    H_xx_nkdd, H_xu_nkdf, H_uu_nkff, J_x_nkd, J_u_nkf = cost_fn.quad_coeffs(trajectory)
+    H_xx_nkdd, H_xu_nkdf, H_uu_nkff, J_x_nkd, J_u_nkf = cost_fn.quad_coeffs(
+        trajectory)
     assert(np.equal(H_xx_nkdd[0, 0], np.eye(x_dim)).all())
-    assert((H_xu_nkdf.numpy() == 0.).all())
-    assert(np.equal(H_uu_nkff[0, 0], np.eye(u_dim)*b).all())
+    assert((H_xu_nkdf == 0.).all())
+    assert(np.equal(H_uu_nkff[0, 0], np.eye(u_dim) * b).all())
 
 
 if __name__ == '__main__':
     test_quad_cost_with_wrapping()
+    print("All tests passed!")
