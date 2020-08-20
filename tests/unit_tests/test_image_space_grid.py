546e0794fa075c99553d21234f10a8f21aadef85
GustavoSilvera <gustavo.r.silvera@gmail.com>
2020-08-19 19:24:00 -0700

    passed test_image_space_grid unit tests

--- tests/unit_tests/test_image_space_grid.py
+++ tests/unit_tests/test_image_space_grid.py
@@ -19,13 +19,13 @@ def create_params():
     p.projected_grid_params = DotMap(
         # Focal length in meters
         f=1.,
-    
+
         # Half-field of view
         fov=np.pi / 4,
-    
+
         # Height of the camera from the ground in meters
         h=1.,
-        
+
         # Tilt of the camera
         tilt=0.
     )
@@ -35,19 +35,19 @@ def create_params():
 def test_image_space_grid():
     # Create parameters
     p = create_params()
-    
+
     # Initialize and Create a grid
     grid = p.grid(p)
 
     # Check if the image bounds are correct
     assert np.isclose(p.bound_min[0], -1., 1e-3)
     assert np.isclose(p.bound_max[0], 1., 1e-3)
-    
+
     # Define some waypoints in the image space
     waypt_image_space = np.array([[1., 0.5, 0.],
-                                  [1., 1., np.pi/4],
-                                  [-1., 0.5, -np.pi/4]])[:, :, np.newaxis]
-    
+                                  [1., 1., np.pi / 4],
+                                  [-1., 0.5, -np.pi / 4]])[:, :, np.newaxis]
+
     # Corresponding waypoints in the world frame (computed using the projection equation, except the angle)
     waypt_world_space = np.array([[2., -2., 0.],
                                   [1., -1., 0.],
@@ -56,28 +56,34 @@ def test_image_space_grid():
     # Check using the projection functions
     waypt_world_space_estimated_x, waypt_world_space_estimated_y, waypt_world_space_estimated_theta, _, _ = \
         grid.generate_worldframe_waypoints_from_imageframe_waypoints(waypt_image_space[:, 0:1, :],
-                                                                     waypt_image_space[:, 1:2, :],
+                                                                     waypt_image_space[:,
+                                                                                       1:2, :],
                                                                      waypt_image_space[:, 2:3, :])
     waypt_image_space_estimated_x, waypt_image_space_estimated_y, waypt_image_space_estimated_theta, _, _ = \
         grid.generate_imageframe_waypoints_from_worldframe_waypoints(waypt_world_space_estimated_x,
                                                                      waypt_world_space_estimated_y,
                                                                      waypt_world_space_estimated_theta)
     # Assert image and world frame x, y points
-    assert np.allclose(waypt_image_space_estimated_x[:, 0, 0], waypt_image_space[:, 0, 0], 1e-3)
-    assert np.allclose(waypt_image_space_estimated_y[:, 0, 0], waypt_image_space[:, 1, 0], 1e-3)
-    assert np.allclose(waypt_image_space_estimated_theta[:, 0, 0], waypt_image_space[:, 2, 0], 1e-3)
-    assert np.allclose(waypt_world_space_estimated_x[:, 0, 0], waypt_world_space[:, 0, 0], 1e-3)
-    assert np.allclose(waypt_world_space_estimated_y[:, 0, 0], waypt_world_space[:, 1, 0], 1e-3)
+    assert np.allclose(
+        waypt_image_space_estimated_x[:, 0, 0], waypt_image_space[:, 0, 0], 1e-3)
+    assert np.allclose(
+        waypt_image_space_estimated_y[:, 0, 0], waypt_image_space[:, 1, 0], 1e-3)
+    assert np.allclose(
+        waypt_image_space_estimated_theta[:, 0, 0], waypt_image_space[:, 2, 0], 1e-3)
+    assert np.allclose(
+        waypt_world_space_estimated_x[:, 0, 0], waypt_world_space[:, 0, 0], 1e-3)
+    assert np.allclose(
+        waypt_world_space_estimated_y[:, 0, 0], waypt_world_space[:, 1, 0], 1e-3)
 
 
 def visualize_world_space_grid():
     # Create parameters
     p = create_params()
-    
+
     # Initialize and Create a grid
     grid = p.grid(p)
     wx_n11, wy_n11, wtheta_n11, _, _ = grid.sample_egocentric_waypoints()
-    
+
     # Plot the waypoints
     import matplotlib.pyplot as plt
     fig = plt.figure()
@@ -95,7 +101,9 @@ def visualize_world_space_grid():
     ax4.hist(wtheta_n11[:, 0, 0])
     fig.savefig('projected_waypoints.pdf')
 
+
 if __name__ == '__main__':
     np.random.seed(seed=1)
     test_image_space_grid()
     # visualize_world_space_grid()
+    print("All tests passed!")
