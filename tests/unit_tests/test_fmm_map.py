afb3641c3383c9eb9b7f62bd37afbfedd3e1bc53
GustavoSilvera <gustavo.r.silvera@gmail.com>
2020-08-19 19:13:24 -0700

    passed test_fmm_map unit tests

--- tests/unit_tests/test_fmm_map.py
+++ tests/unit_tests/test_fmm_map.py
@@ -1,5 +1,5 @@
 import numpy as np
-import tensorflow as tf
+
 
 def test_fmm_map():
     from utils.fmm_map import FmmMap
@@ -15,25 +15,28 @@ def test_fmm_map():
                                                            )
 
     # Let's have a bunch of points to test the fmm distance and angle map
-    test_positions = tf.constant([[[2.0, 3.0], [2.5, 3.0], [3.0, 3.0], [3.0, 2.0], [2.5, 2.0], [2.0, 2.0]]],
-                                 dtype=tf.float32)
+    test_positions = np.array([[[2.0, 3.0], [2.5, 3.0], [3.0, 3.0], [3.0, 2.0], [2.5, 2.0], [2.0, 2.0]]],
+                              dtype=np.float32)
 
     # Predicted distance and angles
-    distances = fmm_map.fmm_distance_map.compute_voxel_function(test_positions, invalid_value=-100.)
-    angles = fmm_map.fmm_angle_map.compute_voxel_function(test_positions, invalid_value=-100.)
+    distances = fmm_map.fmm_distance_map.compute_voxel_function(
+        test_positions, invalid_value=-100.)
+    angles = fmm_map.fmm_angle_map.compute_voxel_function(
+        test_positions, invalid_value=-100.)
 
     # The expected distance is dist1 as defined below. However, due to the numerical issues, the actual computed
     # distance turns out to be a larger (0.6 in this case).
-    dist1 = scale * np.sqrt(2.) - 0.5*scale*np.cos(np.pi*45./180.)
+    dist1 = scale * np.sqrt(2.) - 0.5 * scale * np.cos(np.pi * 45. / 180.)
     dist1 = 0.60
     expected_distances = np.array([dist1, 0.25, dist1, dist1, 0.25, dist1])
-    expected_angles = (np.pi/180) * np.array([-45., -90., -135., 135., 90., 45.])
+    expected_angles = (np.pi / 180) * \
+        np.array([-45., -90., -135., 135., 90., 45.])
 
     assert np.sum(abs(expected_distances - distances) <= 0.01) == 6
     assert np.sum(abs(expected_angles - angles) <= 0.01) == 6
 
 
 if __name__ == '__main__':
-    tf.enable_eager_execution()
     np.random.seed(seed=1)
     test_fmm_map()
+    print("All tests passed!")
