fixed image scaling and moved rendering functions to img_utils

simulators/central_simulator.py
@@ -1,6 +1,3 @@
-import matplotlib as mpl
-mpl.use('Agg')  # for rendering without a display
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import time
@@ -33,7 +30,7 @@ class CentralSimulator(SimulatorHelper):
         self.params = create_simulator_params(render_3D=render_3D)
         self.episode_params = episode_params
         # name of the directory to output everything
-        self.output_directory = \
+        self.params.output_directory = \
             os.path.join(self.params.socnav_params.socnav_dir,
                          "tests/socnav/" + episode_params.name + "_output")
         CentralSimulator.obstacle_map = self._init_obstacle_map(renderer)
@@ -209,8 +206,9 @@ class CentralSimulator(SimulatorHelper):
             del(p)
 
         # capture final wall clock (completion) time
-        wall_clock = time.time() - start_time
-        print("\nSimulation completed in", wall_clock, "real world seconds")
+        self.sim_wall_clock = time.time() - start_time
+        print("\nSimulation completed in",
+              self.sim_wall_clock, "real world seconds")
 
         if(self.episode_params.write_episode_log):
             self.generate_episode_log()
@@ -309,10 +307,10 @@ class CentralSimulator(SimulatorHelper):
             frame = 0
             for p, s in enumerate(self.states.values()):
                 if(skip == 0):
-                    # pool.apply_async(self.convert_state_to_frame, args=(s, filename + str(p) + ".png"))
+                    # pool.apply_async(self.render_sim_state, args=(s, filename + str(p) + ".png"))
                     frame += 1
                     gif_processes.append(multiprocessing.Process(
-                        target=self.convert_state_to_frame,
+                        target=self.render_sim_state,
                         args=(s, filename + str(p) + ".png"))
                     )
                     gif_processes[-1].start()
@@ -335,8 +333,7 @@ class CentralSimulator(SimulatorHelper):
             frame = 0
             for s in self.states.values():
                 if(skip == 0):
-                    self.convert_state_to_frame(
-                        s, filename + str(frame) + ".png")
+                    self.render_sim_state(s, filename + str(frame) + ".png")
                     frame += 1
                     print("Generated Frames:", frame, "out of", num_frames,
                           "%.3f" % (frame / num_frames), "\r", end="")
@@ -357,177 +354,10 @@ class CentralSimulator(SimulatorHelper):
         # fps = 1 / duration # where the duration is the simulation capture rate
         duration = self.delta_t * (1.0 / self.params.fps_scale_down)
         # sequentially
-        save_to_gif(self.output_directory, duration, gif_filename="movie_%d" % (get_seed()),
+        save_to_gif(self.params.output_directory, duration, gif_filename="movie_%d" % (get_seed()),
                     clear_old_files=clear_old_files)
 
-    def plot_topview(self, ax, extent, traversible, human_traversible, camera_pos_13,
-                     pedestrians, robots, room_center, plot_quiver=False, plot_meter_tick=False):
-        """Uses matplotlib to plot a birds-eye-view image of the world by plotting the environment
-        and the gen_agents on every frame. The frame also includes the simulator time and wall clock time
-
-        Args:
-            ax (matplotlib.axes): the axes to plot on
-            extent (np.array): the real_world extent (in meters) of the traversible
-            traversible (np.array): the environment traversible (map bitmap)
-            human_traversible (np.array/None): the human traversibles (or None for non-3D-plots)
-            camera_pos_13 (np.array): the position of the camera (and robot)
-            agents (AgentState dict): the agent states
-            prerecs (HumanState dict): the prerecorded agent states
-            robots (AgentState dict): the robots states
-            room_center (np.array): the center of the "room" to focus the image plot off of
-            plot_quiver (bool, optional): whether or not to plot the quiver (arrow). Defaults to False.
-        """
-        # get number of pixels per meter based off the ax plot space
-        img_scale = ax.transData.transform(
-            (0, 1)) - ax.transData.transform((0, 0))
-        # number of pixels per "meter" unit in the plot
-        ppm = int(img_scale[1] * (1.0 / self.params.img_scale))
-        ax.imshow(traversible, extent=extent, cmap='gray',
-                  vmin=-0.5, vmax=1.5, origin='lower')
-        # Plot human traversible
-        if human_traversible is not None:
-            # NOTE: the human radius is only available given the openGL human modeling
-            # and rendering, thus p.render_with_display must be True
-            # Plot the 5x5 meter human radius grid atop the environment traversible
-            alphas = np.empty(np.shape(human_traversible))
-            for y in range(human_traversible.shape[1]):
-                for x in range(human_traversible.shape[0]):
-                    alphas[x][y] = not(human_traversible[x][y])
-            ax.imshow(human_traversible, extent=extent, cmap='autumn_r',
-                      vmin=-.5, vmax=1.5, origin='lower', alpha=alphas)
-            # alphas = np.all(np.logical_not(human_traversible))
-
-        # TODO: make plot_quiver a simulator-wide param for pedestrians and robot
-        # Plot the camera (robots)
-        plot_agent_dict(ax, ppm, robots, label="Robot", normal_color="bo",
-                        collided_color="ko", plot_quiver=plot_quiver, plot_start_goal=True)
-
-        # plot all the simulated pedestrian agents
-        plot_agent_dict(ax, ppm, pedestrians, label="Pedestrian", normal_color="co",
-                        collided_color="ro", plot_quiver=plot_quiver, plot_start_goal=False)
-
-        if(plot_meter_tick):
-            # plot other useful informational visuals in the topview
-            # such as the key to the length of a "meter" unit
-            plot_line_loc = room_center[:2] * 0.65
-            start = [0, 0] + plot_line_loc
-            end = [1, 0] + plot_line_loc
-            gather_xs = [start[0], end[0]]
-            gather_ys = [start[1], end[1]]
-            col = 'k-'
-            h = 0.1  # height of the "ticks" of the key
-            ax.plot(gather_xs, gather_ys, col)  # main line
-            ax.plot([start[0], start[0]], [start[1] +
-                                           h, start[1] - h], col)  # tick left
-            ax.plot([end[0], end[0]], [end[1] + h,
-                                       end[1] - h], col)  # tick right
-            if(plot_quiver):
-                ax.text(0.5 * (start[0] + end[0]) - 0.2, start[1] +
-                        0.5, "1m", fontsize=14, verticalalignment='top')
-
-    def plot_images(self, p, rgb_image_1mk3, depth_image_1mk1, environment,
-                    camera_pos_13, pedestrians, robots,
-                    sim_t: float, wall_t: float, filename: str, with_zoom=False):
-        """Plots a single frame from information provided about the world state
-
-        Args:
-            p (Map): CentralSimulator params
-            rgb_image_1mk3: the RGB image of the world_state to plot
-            depth_image_1mk1: the depth-map image of the world_state to plot
-            environment (dict): dictionary housing the obj map (bitmap) and more
-            camera_pos_13 (np.array): the position of the camera (and robot)
-            agents (AgentState dict): the agent states
-            prerecs (HumanState dict): the prerecorded agent states
-            robots (AgentState dict): the robots states
-            sim_t (float): the simulator time in seconds
-            wall_t (float): the wall clock time in seconds
-            filename (str): the name of the file to save
-        """
-        map_scale = environment["map_scale"]
-        room_center = environment["room_center"]
-        # Obstacles/building traversible
-        traversible = environment["map_traversible"]
-        human_traversible = None
-        if "human_traversible" in environment.keys():
-            assert(p.render_3D)
-            human_traversible = environment["human_traversible"]
-        # Compute the real_world extent (in meters) of the traversible
-        extent = [0., traversible.shape[1], 0., traversible.shape[0]]
-        extent = np.array(extent) * map_scale
-
-        # count used to signify the number of images that will be generated in a single frame
-        # default 1, for normal view (can add a second for zoomed view)
-        plot_count = 1
-        if(with_zoom):
-            plot_count += 1
-            zoomed_img_plt_indx = plot_count  # 2
-        if rgb_image_1mk3 is not None:
-            plot_count = plot_count + 1
-            rgb_img_plt_indx = plot_count  # 3
-        if depth_image_1mk1 is not None:
-            plot_count = plot_count + 1
-            depth_img_plt_indx = plot_count  # 4
-
-        img_size = 10 * p.img_scale
-        fig = plt.figure(figsize=(plot_count * img_size, img_size))
-        ax = fig.add_subplot(1, plot_count, 1)
-        ax.set_xlim(0., traversible.shape[1] * map_scale)
-        ax.set_ylim(0., traversible.shape[0] * map_scale)
-        self.plot_topview(ax, extent, traversible, human_traversible,
-                          camera_pos_13, pedestrians, robots, room_center, plot_quiver=True)
-        ax.legend()
-        time_string = "sim_t=%.3f" % sim_t + " wall_t=%.3f" % wall_t
-        ax.set_title(time_string, fontsize=20)
-
-        if(with_zoom):
-            # Plot the 5x5 meter occupancy grid centered around the camera
-            zoom = 8.5  # zoom out in by a constant amount
-            ax = fig.add_subplot(1, plot_count, zoomed_img_plt_indx)
-            ax.set_xlim([room_center[0] - zoom, room_center[0] + zoom])
-            ax.set_ylim([room_center[1] - zoom, room_center[1] + zoom])
-            self.plot_topview(ax, extent, traversible, human_traversible,
-                              camera_pos_13, pedestrians, robots, room_center, plot_quiver=True)
-            ax.legend()
-            ax.set_xticks([])
-            ax.set_yticks([])
-            ax.set_title(time_string, fontsize=20)
-
-        if rgb_image_1mk3 is not None:
-            # Plot the RGB Image
-            ax = fig.add_subplot(1, plot_count, rgb_img_plt_indx)
-            ax.imshow(rgb_image_1mk3[0].astype(np.uint8))
-            ax.set_xticks([])
-            ax.set_yticks([])
-            ax.set_title('RGB')
-
-        if depth_image_1mk1 is not None:
-            # Plot the Depth Image
-            ax = fig.add_subplot(1, plot_count, depth_img_plt_indx)
-            ax.imshow(depth_image_1mk1[0, :, :, 0].astype(
-                np.uint8), cmap='gray')
-            ax.set_xticks([])
-            ax.set_yticks([])
-            ax.set_title('Depth')
-
-        full_file_name = os.path.join(self.output_directory, filename)
-        if(not os.path.exists(full_file_name)):
-            if(self.params.verbose_printing):
-                print('\033[31m', "Failed to find:", full_file_name,
-                      '\033[33m', "and therefore it will be created", '\033[0m')
-            touch(full_file_name)  # Just as the bash command
-
-        fig.savefig(full_file_name, bbox_inches='tight', pad_inches=0)
-        fig.clear()
-        plt.cla()
-        plt.clf()
-        plt.close('all')
-        plt.close(fig)
-        del fig
-        if(self.params.verbose_printing):
-            print('\033[32m', "Successfully rendered:",
-                  full_file_name, '\033[0m')
-
-    def convert_state_to_frame(self, state: SimState, filename: str):
+    def render_sim_state(self, state: SimState, filename: str):
         """Converts a state into an image to be later converted to a gif movie
 
         Args:
@@ -561,10 +391,10 @@ class CentralSimulator(SimulatorHelper):
                                      state.get_environment()["map_scale"],
                                      human_visible=True)
         # plot the rbg, depth, and topview images if applicable
-        self.plot_images(self.params, rgb_image_1mk3, depth_image_1mk1,
-                         state.get_environment(), camera_pos_13,
-                         state.get_pedestrians(), state.get_robots(),
-                         state.get_sim_t(), state.get_wall_t(), filename)
+        render_scene(self.params, rgb_image_1mk3, depth_image_1mk1,
+                     state.get_environment(), camera_pos_13,
+                     state.get_pedestrians(), state.get_robots(),
+                     state.get_sim_t(), state.get_wall_t(), filename)
         # Delete state to save memory after frames are generated
         del(state)
 
@@ -572,7 +402,7 @@ class CentralSimulator(SimulatorHelper):
 
     def generate_episode_log(self, filename='episode_log.txt'):
         import io
-        abs_filename = os.path.join(self.output_directory, filename)
+        abs_filename = os.path.join(self.params.output_directory, filename)
         touch(abs_filename)  # create if dosent already exist
         ep_params = self.episode_params
         data = ""
@@ -586,6 +416,7 @@ class CentralSimulator(SimulatorHelper):
         data += "Total agents in scene: %d\n" % self.total_agents
         data += "****************SIMULATOR INFO****************\n"
         data += "Simulator refresh rate (s): %0.3f\n" % self.delta_t
+        data += "Total duration of simulation (s): %0.3f\n" % self.sim_wall_clock
         num_successful = self.num_completed_agents + self.num_completed_prerecs
         data += "Num Successful agents: %d\n" % num_successful
         num_collision = self.num_collided_agents + self.num_collided_prerecs
@@ -597,7 +428,8 @@ class CentralSimulator(SimulatorHelper):
             data += "Robot termination cause: %s\n" % self.robot.termination_cause
             data += "Num commands received from joystick: %d\n" % len(
                 self.robot.joystick_inputs)
-            data += "Total time (s) blocking for joystick input: %0.3f\n" % self.robot.get_block_t_total()
+            data += "Total time blocking for joystick input (s): %0.3f\n" % self.robot.get_block_t_total(
+            )
             data += "Num commands executed by robot: %d\n" % self.robot.num_executed
             rob_displacement = euclidean_dist2(ep_params.robot_start_goal[0],
                                                self.robot.get_current_config().to_3D_numpy()

utils/image_utils.py
@@ -1,6 +1,9 @@
 import os
 import numpy as np
 from utils.utils import *
+import matplotlib as mpl
+mpl.use('Agg')  # for rendering without a display
+import matplotlib.pyplot as plt
 
 
 def plot_image_observation(ax, img_mkd, size=None):
@@ -43,7 +46,7 @@ def gather_metadata(ppm, a, plot_start_goal, start, goal):
 def gather_colors_and_labels(base_color: str, label: str, has_collided: bool, indx: int):
     color = base_color  # gen_agents are green and solid unless collided
     start_col = 'yo'  # yellow circle
-    goal_col = 'go'   # green circle
+    goal_col = 'g'   # yellow (star)
     if(has_collided):
         color = 'ro'  # collided agents are drawn red
     draw_label = None
@@ -62,7 +65,6 @@ def plot_agent_dict(ax, ppm: float, agents_dict: dict, label='Agent', normal_col
                     plot_start_goal=False, start_3=None, goal_3=None):
     # plot all the simulated prerecorded gen_agents
     for i, a in enumerate(agents_dict.values()):
-
         # gather important info regarding the values to plot
         collided, ms, pos_3, traj_col, start_3, goal_3 = \
             gather_metadata(ppm, a, plot_start_goal, start_3, goal_3)
@@ -80,22 +82,192 @@ def plot_agent_dict(ax, ppm: float, agents_dict: dict, label='Agent', normal_col
 
         # plot start + goal
         if(plot_start_goal):
-            ax.plot(start_3[0], start_3[1], start_col, markersize=ms, label=sl)
-            ax.plot(goal_3[0], goal_3[1], goal_col, markersize=ms, label=gl)
+            ax.plot(start_3[0], start_3[1], start_col,
+                    markersize=ms, label=sl, alpha=0.5)
+            ax.plot(goal_3[0], goal_3[1], goal_col,
+                    markersize=2 * ms, marker="*", label=gl, alpha=0.8)
 
-        # plot the surrounding "force field" around the agent
-        ax.plot(pos_3[0], pos_3[1], color,
-                alpha=0.2, markersize=2. * ms)
+        # plot a surrounding "force field" around the agent
+        # ax.plot(pos_3[0], pos_3[1], color,alpha=0.2, markersize=2. * ms)
         if(plot_quiver):
-            # TODO: use scale_units: xy instead of inches, scale=1
             # Agent heading
-            ax.quiver(pos_3[0], pos_3[1], np.cos(pos_3[2]), np.sin(pos_3[2]),
-                      scale=150.0 / ppm, scale_units='inches')
+            s = 1
+            ax.quiver(pos_3[0], pos_3[1], s * np.cos(pos_3[2]), s * np.sin(pos_3[2]),
+                      scale=1, scale_units='xy')
             if(plot_start_goal and (start_3 is not None and goal_3 is not None)):
-                ax.quiver(start_3[0], start_3[1], np.cos(start_3[2]), np.sin(start_3[2]),
-                          scale=200.0 / ppm, scale_units='inches')
-                ax.quiver(goal_3[0], goal_3[1], np.cos(goal_3[2]), np.sin(goal_3[2]),
-                          scale=200.0 / ppm, scale_units='inches')
+                ax.quiver(start_3[0], start_3[1], s * np.cos(start_3[2]), s * np.sin(start_3[2]),
+                          scale=1, scale_units='xy')
+                ax.quiver(goal_3[0], goal_3[1], s * np.cos(goal_3[2]), s * np.sin(goal_3[2]),
+                          scale=1, scale_units='xy')
+
+
+def plot_topview(ax, extent, traversible, human_traversible, camera_pos_13,
+                 pedestrians, robots, room_center, plot_quiver=False, plot_meter_tick=False):
+    """Uses matplotlib to plot a birds-eye-view image of the world by plotting the environment
+    and the gen_agents on every frame. The frame also includes the simulator time and wall clock time
+
+    Args:
+        ax (matplotlib.axes): the axes to plot on
+        extent (np.array): the real_world extent (in meters) of the traversible
+        traversible (np.array): the environment traversible (map bitmap)
+        human_traversible (np.array/None): the human traversibles (or None for non-3D-plots)
+        camera_pos_13 (np.array): the position of the camera (and robot)
+        agents (AgentState dict): the agent states
+        prerecs (HumanState dict): the prerecorded agent states
+        robots (AgentState dict): the robots states
+        room_center (np.array): the center of the "room" to focus the image plot off of
+        plot_quiver (bool, optional): whether or not to plot the quiver (arrow). Defaults to False.
+    """
+    # get number of pixels-per-meter based off the ax plot space
+    img_scale = \
+        ax.transData.transform((0, 1)) - ax.transData.transform((0, 0))
+    # scale the pixels-per-meter based off the image scale
+    ppm = int(img_scale[1] * 0.5)
+    ax.imshow(traversible, extent=extent, cmap='gray',
+              vmin=-0.5, vmax=1.5, origin='lower')
+    # Plot human traversible
+    if human_traversible is not None:
+        # NOTE: the human radius is only available given the openGL human modeling
+        # and rendering, thus p.render_with_display must be True
+        # Plot the 5x5 meter human radius grid atop the environment traversible
+        alphas = np.empty(np.shape(human_traversible))
+        for y in range(human_traversible.shape[1]):
+            for x in range(human_traversible.shape[0]):
+                alphas[x][y] = not(human_traversible[x][y])
+        ax.imshow(human_traversible, extent=extent, cmap='autumn_r',
+                  vmin=-.5, vmax=1.5, origin='lower', alpha=alphas)
+        # alphas = np.all(np.logical_not(human_traversible))
+
+    # TODO: make plot_quiver a simulator-wide param for pedestrians and robot
+    # Plot the camera (robots)
+    plot_agent_dict(ax, ppm, robots, label="Robot", normal_color="bo",
+                    collided_color="ko", plot_quiver=plot_quiver, plot_start_goal=True)
+
+    # plot all the simulated pedestrian agents
+    plot_agent_dict(ax, ppm, pedestrians, label="Pedestrian", normal_color="co",
+                    collided_color="ro", plot_quiver=plot_quiver, plot_start_goal=False)
+
+    if(plot_meter_tick):
+        # plot other useful informational visuals in the topview
+        # such as the key to the length of a "meter" unit
+        plot_line_loc = room_center[:2] * 0.65
+        start = [0, 0] + plot_line_loc
+        end = [1, 0] + plot_line_loc
+        gather_xs = [start[0], end[0]]
+        gather_ys = [start[1], end[1]]
+        col = 'k-'
+        h = 0.1  # height of the "ticks" of the key
+        ax.plot(gather_xs, gather_ys, col)  # main line
+        ax.plot([start[0], start[0]], [start[1] +
+                                       h, start[1] - h], col)  # tick left
+        ax.plot([end[0], end[0]], [end[1] + h,
+                                   end[1] - h], col)  # tick right
+        if(plot_quiver):
+            ax.text(0.5 * (start[0] + end[0]) - 0.2, start[1] +
+                    0.5, "1m", fontsize=14, verticalalignment='top')
+
+
+def render_scene(p, rgb_image_1mk3, depth_image_1mk1, environment,
+                 camera_pos_13, pedestrians, robots,
+                 sim_t: float, wall_t: float, filename: str, with_zoom=False):
+    """Plots a single frame from information provided about the world state
+
+    Args:
+        p (Map): CentralSimulator params
+        rgb_image_1mk3: the RGB image of the world_state to plot
+        depth_image_1mk1: the depth-map image of the world_state to plot
+        environment (dict): dictionary housing the obj map (bitmap) and more
+        camera_pos_13 (np.array): the position of the camera (and robot)
+        agents (AgentState dict): the agent states
+        prerecs (HumanState dict): the prerecorded agent states
+        robots (AgentState dict): the robots states
+        sim_t (float): the simulator time in seconds
+        wall_t (float): the wall clock time in seconds
+        filename (str): the name of the file to save
+    """
+    map_scale = environment["map_scale"]
+    room_center = environment["room_center"]
+    # Obstacles/building traversible
+    traversible = environment["map_traversible"]
+    human_traversible = None
+    if "human_traversible" in environment.keys():
+        assert(p.render_3D)
+        human_traversible = environment["human_traversible"]
+    # Compute the real_world extent (in meters) of the traversible
+    extent = [0., traversible.shape[1], 0., traversible.shape[0]]
+    extent = np.array(extent) * map_scale
+
+    # count used to signify the number of images that will be generated in a single frame
+    # default 1, for normal view (can add a second for zoomed view)
+    plot_count = 1
+    if(with_zoom):
+        plot_count += 1
+        zoomed_img_plt_indx = plot_count  # 2
+    if rgb_image_1mk3 is not None:
+        plot_count = plot_count + 1
+        rgb_img_plt_indx = plot_count  # 3
+    if depth_image_1mk1 is not None:
+        plot_count = plot_count + 1
+        depth_img_plt_indx = plot_count  # 4
+
+    img_size = 10 * p.img_scale
+    fig = plt.figure(figsize=(plot_count * img_size, img_size))
+    ax = fig.add_subplot(1, plot_count, 1)
+    ax.set_xlim(0., traversible.shape[1] * map_scale)
+    ax.set_ylim(0., traversible.shape[0] * map_scale)
+    plot_topview(ax, extent, traversible, human_traversible,
+                 camera_pos_13, pedestrians, robots, room_center, plot_quiver=True)
+    ax.legend()
+    time_string = "sim_t=%.3f" % sim_t + " wall_t=%.3f" % wall_t
+    ax.set_title(time_string, fontsize=20)
+
+    if(with_zoom):
+        # Plot the 5x5 meter occupancy grid centered around the camera
+        zoom = 8.5  # zoom out in by a constant amount
+        ax = fig.add_subplot(1, plot_count, zoomed_img_plt_indx)
+        ax.set_xlim([room_center[0] - zoom, room_center[0] + zoom])
+        ax.set_ylim([room_center[1] - zoom, room_center[1] + zoom])
+        plot_topview(ax, extent, traversible, human_traversible,
+                     camera_pos_13, pedestrians, robots, room_center, plot_quiver=True)
+        ax.legend()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title(time_string, fontsize=20)
+
+    if rgb_image_1mk3 is not None:
+        # Plot the RGB Image
+        ax = fig.add_subplot(1, plot_count, rgb_img_plt_indx)
+        ax.imshow(rgb_image_1mk3[0].astype(np.uint8))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title('RGB')
+
+    if depth_image_1mk1 is not None:
+        # Plot the Depth Image
+        ax = fig.add_subplot(1, plot_count, depth_img_plt_indx)
+        ax.imshow(depth_image_1mk1[0, :, :, 0].astype(
+            np.uint8), cmap='gray')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title('Depth')
+
+    full_file_name = os.path.join(p.output_directory, filename)
+    if(not os.path.exists(full_file_name)):
+        if(p.verbose_printing):
+            print('\033[31m', "Failed to find:", full_file_name,
+                  '\033[33m', "and therefore it will be created", '\033[0m')
+        touch(full_file_name)  # Just as the bash command
+
+    fig.savefig(full_file_name, bbox_inches='tight', pad_inches=0)
+    fig.clear()
+    plt.cla()
+    plt.clf()
+    plt.close('all')
+    plt.close(fig)
+    del fig
+    if(p.verbose_printing):
+        print('\033[32m', "Successfully rendered:",
+              full_file_name, '\033[0m')
 
 
 def render_rgb_and_depth(r, camera_pos_13, dx_m: float, human_visible=True):