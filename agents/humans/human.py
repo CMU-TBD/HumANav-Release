renamed agents to numerical, fixed utils

agents/humans/human.py
@@ -1,6 +1,6 @@
 from agents.humans.human_appearance import HumanAppearance
 from agents.humans.human_configs import HumanConfigs
-from utils.utils import *
+from utils.utils import generate_name, generate_config_from_pos_3
 from agents.agent import Agent
 import numpy as np
 
@@ -23,11 +23,7 @@ class Human(Agent):
         """
         Sample a new random human from all required features
         """
-        human_name = None
-        if(name is None):
-            human_name = generate_name(max_chars)
-        else:
-            human_name = name
+        human_name = name if name is not None else generate_name(max_chars)
         if(verbose):
             # In order to print more readable arrays
             np.set_printoptions(precision=2)
@@ -72,3 +68,31 @@ class Human(Agent):
                 HumanAppearance)
         configs = HumanConfigs.generate_random_human_config(environment)
         return Human.generate_human(appearance, configs)
+
+    @staticmethod
+    def generate(simulator, p, starts, goals, environment, r):
+        """
+        Generate and add num_humans number of randomly generated humans to the simulator
+        """
+        num_gen_humans = min(len(starts), len(goals))
+        print("Generated Auto Humans:", num_gen_humans)
+        from agents.humans.human_configs import HumanConfigs
+        for i in range(num_gen_humans):
+            start_config = generate_config_from_pos_3(starts[i])
+            goal_config = generate_config_from_pos_3(goals[i])
+            start_goal_configs = HumanConfigs(start_config, goal_config)
+            human_i_name = "auto_%04d" % i
+            # Generates a random human from the environment
+            new_human_i = Human.generate_human_with_configs(
+                start_goal_configs,
+                generate_appearance=p.render_3D,
+                name=human_i_name
+            )
+            # update renderer and get human traversible if it exists
+            if p.render_3D:
+                r.add_human(new_human_i)
+                environment["human_traversible"] = \
+                    np.array(r.get_human_traversible())
+
+            # Input human fields into simulator
+            simulator.add_agent(new_human_i)

agents/humans/recorded_human.py
@@ -1,4 +1,6 @@
-from utils.utils import *
+from utils.utils import generate_config_from_pos_3, euclidean_dist2
+from utils.utils import color_red, color_reset
+import os
 from agents.humans.human import Human
 from agents.humans.human_configs import HumanConfigs
 from agents.humans.human_appearance import HumanAppearance
@@ -55,7 +57,7 @@ class PrerecordedHuman(Human):
         t = self.current_precalc_step
         prev_x, prev_y, prev_theta = \
             np.squeeze(self.posn_data[t].position_and_heading_nk3())
-        next_x, next_y, next_theta = \
+        _, _, next_theta = \
             np.squeeze(self.posn_data[t + 1].position_and_heading_nk3())
         # TODO: fix bug where interpolated points VERY close to the previous points
         # result in numerical errors that result in incorrect theta calculations
@@ -213,12 +215,8 @@ class PrerecordedHuman(Human):
         return config_data
 
     @staticmethod
-    def generate_pedestrians(simulator, params,
-                             max_time: int = 10e7,
-                             start_t: float = 0,
-                             ped_range: tuple = (0, -1),
-                             dataset: DotMap = None
-                             ):
+    def generate(simulator, params, environment, renderer, max_time=10e7,
+                 start_t=0, ped_range=(0, -1), dataset=None):
         """"world_df" is a set of trajectories organized as a pandas dataframe.
             Each row is a pedestrian at a given frame (aka time point).
             The data was taken at 25 fps so between frames is 1/25th of a second. """
@@ -236,59 +234,64 @@ class PrerecordedHuman(Human):
         scale_x = -1 if dataset.flipxn else 1
         scale_y = -1 if dataset.flipyn else 1
         # run through the amount of agents
-        if ped_range[0] != ped_range[1]:  # have a non-empty range
-            datafile = \
-                os.path.join(params.socnav_dir, params.dataset_dir, csv_file)
-            world_df = pd.read_csv(datafile, header=None).T
-            world_df.columns = ['frame', 'ped', 'y', 'x']
-            world_df[['frame', 'ped']] = \
-                world_df[['frame', 'ped']].astype('int')
-            start_frame = world_df['frame'][0]  # default start (of data)
-            all_peds = np.unique(world_df.ped)
-            max_peds = max(all_peds)
-            if max_agents == -1:
-                # set to all pedestrians
-                max_agents = max_peds - 1
-            # ensure that max_agents never goes out of bounds
-            max_agents = min(max_agents, max_peds)
-            for i in range(max_agents):
-                ped_id = i + start_idx + 1
-                if ped_id not in all_peds:
-                    print("%sRequested agent %d not found in dataset: %s%s" %
-                          (color_red, ped_id, csv_file, color_reset))
-                    # this can happen based off the dataset
-                    continue
-                ped_i = world_df[world_df.ped == ped_id]
-                # gather data
-                if i == 0:
-                    # update start frame to be representative of "first" pedestrian
-                    start_frame = list(ped_i['frame'])[0]
-                t_data = PrerecordedHuman.gather_times(ped_i, spawn_delay_s, start_t,
-                                                       start_frame, fps)
-                if (ped_i.frame.iloc[0] - start_frame) / fps > max_time:
-                    # assuming the data of the agents is sorted relatively based off time
-                    break
-                print("Generating pedestrians from \"%s\" in range [%d, %d]: %d\r" %
-                      (dataset.name, ped_range[0], ped_range[1], ped_id), end="")
-                xytheta_data = PrerecordedHuman.gather_posn_data(ped_i, offset,
-                                                                 swap_axes=swapxy,
-                                                                 scale_x=scale_x,
-                                                                 scale_y=scale_y)
-                interp_fns = PrerecordedHuman.init_interp_fns(xytheta_data,
-                                                              t_data)
+        if ped_range[0] == ped_range[1]:  # have an empty range
+            return
+        datafile = \
+            os.path.join(params.socnav_dir, params.dataset_dir, csv_file)
+        print("Generating pedestrians from \"%s\" in range [%d, %d]\r" %
+              (dataset.name, ped_range[0], ped_range[1]), end="")
+        world_df = pd.read_csv(datafile, header=None).T
+        world_df.columns = ['frame', 'ped', 'y', 'x']
+        world_df[['frame', 'ped']] = \
+            world_df[['frame', 'ped']].astype('int')
+        start_frame = world_df['frame'][0]  # default start (of data)
+        all_peds = np.unique(world_df.ped)
+        max_peds = max(all_peds)
+        if max_agents == -1:
+            # set to all pedestrians
+            max_agents = max_peds - 1
+        # ensure that max_agents never goes out of bounds
+        max_agents = min(max_agents, max_peds)
+        for i in range(max_agents):
+            ped_id = i + start_idx + 1
+            if ped_id not in all_peds:
+                print("%sRequested agent %d not found in dataset: %s%s" %
+                      (color_red, ped_id, csv_file, color_reset))
+                # this can happen based off the dataset
+                continue
+            ped_i = world_df[world_df.ped == ped_id]
+            # gather data
+            if i == 0:
+                # update start frame to be representative of "first" pedestrian
+                start_frame = list(ped_i['frame'])[0]
+            t_data = PrerecordedHuman.gather_times(ped_i, spawn_delay_s, start_t,
+                                                   start_frame, fps)
+            if (ped_i.frame.iloc[0] - start_frame) / fps > max_time:
+                # assuming the data of the agents is sorted relatively based off time
+                break
+            print("Generating pedestrians from \"%s\" in range [%d, %d]: %d\r" %
+                  (dataset.name, ped_range[0], ped_range[1], ped_id), end="")
+            xytheta_data = PrerecordedHuman.gather_posn_data(ped_i, offset,
+                                                             swap_axes=swapxy,
+                                                             scale_x=scale_x,
+                                                             scale_y=scale_y)
+            interp_fns = PrerecordedHuman.init_interp_fns(xytheta_data,
+                                                          t_data)
 
-                v_data = PrerecordedHuman.gather_vel_data(t_data, xytheta_data)
-                # combine the xytheta with the velocity
-                config_data = PrerecordedHuman.to_configs(xytheta_data, v_data)
-                new_agent = PrerecordedHuman(t_data=t_data, posn_data=config_data,
-                                             generate_appearance=params.render_3D,
-                                             interps=interp_fns)
-                simulator.add_agent(new_agent)
+            v_data = PrerecordedHuman.gather_vel_data(t_data, xytheta_data)
+            # combine the xytheta with the velocity
+            config_data = PrerecordedHuman.to_configs(xytheta_data, v_data)
+            name = "prerec_%04d" % (i)
+            new_agent = PrerecordedHuman(t_data=t_data, posn_data=config_data,
+                                         generate_appearance=params.render_3D,
+                                         interps=interp_fns, name=name)
+            simulator.add_agent(new_agent)
 
-                # add human to renderer
-                # if params.render_3D:
-                #     renderer.add_human(new_agent)
-                #     environment["human_traversible"] = \
-                #         np.array(renderer.get_human_traversible())
-            # to not disturb the carriage-return print
-            print()
+            # add human to renderer
+            # TODO: make sure this works
+            if params.render_3D:
+                renderer.add_human(new_agent)
+                environment["human_traversible"] = \
+                    np.array(renderer.get_human_traversible())
+        # to not disturb the carriage-return print
+        print()

agents/robot_agent.py
@@ -1,6 +1,7 @@
-from utils.utils import *
+from utils.utils import generate_config_from_pos_3
 from agents.agent import Agent
-from agents.robot_utils import *
+from agents.robot_utils import clip_vel, clip_posn, send_sim_state, send_to_joystick, force_connect
+from agents.robot_utils import establish_handshake, listen_once, close_sockets
 from trajectory.trajectory import SystemConfig
 from params.central_params import create_robot_params
 import numpy as np
@@ -8,10 +9,9 @@ import time
 
 
 class RobotAgent(Agent):
-    def __init__(self, name, start_configs):
-        self.name = name
-        super().__init__(start_configs.get_start_config(),
-                         start_configs.get_goal_config(),
+    def __init__(self, name, start_config, goal_config):
+        super().__init__(start_config,
+                         goal_config,
                          name)
         self.joystick_inputs = []
         # josystick is ready once it has been sent an environment
@@ -56,18 +56,34 @@ class RobotAgent(Agent):
         return self.block_time_total
 
     @staticmethod
-    def generate_robot(configs, name=None, verbose=False):
+    def generate(simulator, params, robot_start_goal):
+        assert(len(robot_start_goal) == 2)
+        rob_start = generate_config_from_pos_3(robot_start_goal[0])
+        rob_goal = generate_config_from_pos_3(robot_start_goal[1])
+        robot_agent = RobotAgent.generate_robot_with_start_goal(rob_start,
+                                                                rob_goal)
+        simulator.add_agent(robot_agent)
+
+    @staticmethod
+    def generate_robot(configs, verbose=False):
+        """
+        Sample a new random robot agent from all required features
+        """
+        start = configs.get_start_config().to_3D_numpy()
+        goal = configs.get_goal_config().to_3D_numpy()
+        return RobotAgent.generate_robot_with_start_goal(start, goal, verbose=verbose)
+
+    @staticmethod
+    def generate_robot_with_start_goal(start, goal, verbose=False):
         """
         Sample a new random robot agent from all required features
         """
         robot_name = "robot_agent"  # constant name for the robot since there will only ever be one
         # In order to print more readable arrays
         np.set_printoptions(precision=2)
-        pos_2 = configs.get_start_config().to_3D_numpy()
-        goal_2 = configs.get_goal_config().to_3D_numpy()
         if verbose:
-            print("Robot", robot_name, "at", pos_2, "with goal", goal_2)
-        return RobotAgent(robot_name, configs)
+            print("Robot", robot_name, "at", start, "with goal", goal)
+        return RobotAgent(robot_name, start, goal)
 
     @staticmethod
     def random_from_environment(environment):